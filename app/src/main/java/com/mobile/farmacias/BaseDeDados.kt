ff19f21d3b3036e187b0237104f9da88ade6bf9c
DeckHunter <38442904+DeckHunter@users.noreply.github.com>
2021-08-30 00:16:50 -0300

    Update BaseDeDados.kt

--- app/src/main/java/com/mobile/farmacias/BaseDeDados.kt
+++ app/src/main/java/com/mobile/farmacias/BaseDeDados.kt
@@ -3,13 +3,13 @@ package com.mobile.farmacias.data
 import android.content.Context
 import com.google.android.gms.maps.model.LatLng
 import com.mobile.farmacias.R
-import com.mobile.farmacias.model.DrugStore
+import com.mobile.farmacias.model.Farmacias
 import java.util.*
 
 class BaseDeDados {
     companion object {
-        fun loadDrugStores(context: Context): List<DrugStore> {
-            val drugStores = mutableListOf<DrugStore>()
+        fun carregarFarmacias(context: Context): List<Farmacias> {
+            val farmacias = mutableListOf<Farmacias>()
             listOf(R.array.pague_menos, R.array.extra_frama, R.array.drogasil)
                 .forEach { storeNameId ->
                     Arrays.asList(context.resources.getStringArray(storeNameId))
@@ -17,15 +17,15 @@ class BaseDeDados {
                             it.forEach {
                                 val position = it.toString().split(",")
                                 val latlng = LatLng(position[0].toDouble(), position[1].toDouble())
-                                val drugStore = DrugStore(
+                                val farmacia = Farmacias(
                                     context.resources.getResourceEntryName(storeNameId),
                                     latlng
                                 )
-                                drugStores.add(drugStore)
+                                farmacias.add(farmacia)
                             }
                         }
                 }
-            return drugStores
+            return farmacias
         }
     }
 }
