81b5d27c34f7cd975cfb50b4111f0dec9d390916
DeckHunter <38442904+DeckHunter@users.noreply.github.com>
2021-08-30 00:12:24 -0300

    Update and rename DrugStore.kt to Farmacias.kt

--- app/src/main/java/com/mobile/farmacias/model/Farmacias.kt
+++ app/src/main/java/com/mobile/farmacias/model/Farmacias.kt
@@ -2,7 +2,7 @@ package com.mobile.farmacias.model
 
 import com.google.android.gms.maps.model.LatLng
 
-data class DrugStore(
+data class Farmacias(
     val name: String,
     val position: LatLng
 )
